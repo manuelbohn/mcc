0818b0452465ee2bb178a60419af215d22df34a4
manuelbohn <bohn@stanford.edu>
2018-04-20 12:09:31 -0700

    results from ex3

--- stats/data_preprocessing_Ex3.R
+++ stats/data_preprocessing_Ex3.R
@@ -16,13 +16,14 @@ setwd("~/Work/MCC/git-mcc/mcc/stats/")
 
 
 # select all files from individual workers
-files <- dir("~/Work/MCC/git-mcc/cosub_Ex3_novel/sandbox-results")
+#files <- dir("~/Work/MCC/git-mcc/cosub_Ex3_novel/sandbox-results")
+files <- dir("~/Work/MCC/git-mcc/cosub_Ex3_novel/production-results")
 
 #combine files into one dataframe
 raw <- data.frame()
 for (f in files) {
-  jf <- paste("~/Work/MCC/git-mcc/cosub_novelty/anonymized-results/",f,sep="")
-  
+  #jf <- paste("~/Work/MCC/git-mcc/cosub_Ex3_novel/sandbox-results/",f,sep="")
+  jf <- paste("~/Work/MCC/git-mcc/cosub_Ex3_novel/production-results/",f,sep="")
   jd <- fromJSON(paste(readLines(jf), collapse=""))
   id <- data.frame(workerid = jd$WorkerId, 
                    data = jd$answers$data$data
@@ -31,17 +32,21 @@ for (f in files) {
 }
 
 # convert into short format, drop unnecessary columns, rename variables and sort by id
-novel.data= melt(setDT(raw), measure = patterns( "^data.condition","^data.agent","^data.altAgent","^data.leftFruit","^data.rightFruit","^data.pick","^data.novel","^data.trial","^data.speakerChange","^data.rt", "^data.correct"))
-names(novel.data) = c("id","alltrial","condition","agent","altAgent","leftObject","rightObject","pick","target","trial","change","rt","correct") 
-novel.data$pick= str_sub(novel.data$pick,73,str_length(novel.data$pick)-4)
+novel.data= melt(setDT(raw), measure = patterns( "^data.experiment","^data.agent","^data.altAgent","^data.rightFruit","^data.leftFruit","^data.corrFruit_inf","^data.corrFruit_novel","^data.pick","^data.novel","^data.inf","^data.trial","^data.alignment","^data.speaker","^data.rt", "^data.correct_inf","^data.correct_novel"))
+names(novel.data) = c("id","alltrial","experiment","agent","altAgent","rightFruit","leftFruit","corObjInf","corObjPrior","pick","prior","inf","trial","alignment","change","rt","correct_inf","correct_prior") 
+novel.data$pick= str_sub(novel.data$pick,63,str_length(novel.data$pick)-4)
 novel.data = novel.data[!duplicated(novel.data), ]
 novel.data = novel.data[order(id)]
 novel.data$id = paste(novel.data$id,novel.data$condition,sep="_")
+# Problem with t1: is seen as correct from both perspectives if other object is t10 -t18. Recode this
+novel.data <- novel.data %>%
+  mutate(correct_inf = ifelse(pick == corObjInf,1,0),
+         correct_prior = ifelse(pick == corObjPrior,1,0)) 
 # check resulting datafile
 str(novel.data)
 head(novel.data)
 # write csv file for further analysis
-write.csv(novel.data, file="novel.data.csv")
+write.csv(novel.data, file="ex3.novel.data.csv")
 
 ################################################################################################################
 
@@ -51,12 +56,13 @@ write.csv(novel.data, file="novel.data.csv")
 ################################################################################################################
 
 
-files <- dir("~/Work/MCC/git-mcc/cosub_Ex3_pref/sandbox-results")
+#files <- dir("~/Work/MCC/git-mcc/cosub_Ex3_pref/sandbox-results")
+files <- dir("~/Work/MCC/git-mcc/cosub_Ex3_pref/production-results")
 
 #combine files into one dataframe
 raw <- data.frame()
 for (f in files) {
-  jf <- paste("~/Work/MCC/git-mcc/cosub_Ex3_pref/sandbox-results/",f,sep="")
+  jf <- paste("~/Work/MCC/git-mcc/cosub_Ex3_pref/production-results/",f,sep="")
   jd <- fromJSON(paste(readLines(jf), collapse=""))
   id <- data.frame(workerid = jd$WorkerId, 
                    data = jd$answers$data$data
@@ -65,17 +71,21 @@ for (f in files) {
 }
 
 # convert into short format, drop unnecessary columns, rename variables and sort by id
-pref.data= melt(setDT(raw), measure = patterns( "^data.experiment","^data.agent","^data.altAgent","^data.corrFruit_inf","^data.corrFruit_pref","^data.pick","^data.pref","^data.inf","^data.trial","^data.alignment","^data.speakerChange","^data.rt", "^data.correct_inf","^data.correct_pref"))
-names(pref.data) = c("id","alltrial","experiment","agent","altAgent","cortObjInf","cortObjPref","pick","pref","inf","trial","alignment","change","rt","correct_inf","correct_pref") 
-pref.data$pick= str_sub(pref.data$pick,76,str_length(pref.data$pick)-4)
+pref.data= melt(setDT(raw), measure = patterns( "^data.experiment","^data.agent","^data.altAgent","^data.rightFruit","^data.leftFruit","^data.corrFruit_inf","^data.corrFruit_pref","^data.pick","^data.pref","^data.inf","^data.trial","^data.alignment","^data.speaker","^data.rt", "^data.correct_inf","^data.correct_pref"))
+names(pref.data) = c("id","alltrial","experiment","agent","altAgent","rightFruit","leftFruit","corObjInf","corObjPrior","pick","prior","inf","trial","alignment","change","rt","correct_inf","correct_prior") 
+pref.data$pick= str_sub(pref.data$pick,63,str_length(pref.data$pick)-4)
 pref.data = pref.data[!duplicated(pref.data), ]
 pref.data = pref.data[order(id)]
-pref.data $id = paste(pref.data $id, pref.data $condition,sep="_")
+pref.data $id = paste(pref.data $id, pref.data $experiment,sep="_")
+# Problem with t1: is seen as correct from both perspectives if other object is t10 -t18. Recode this
+pref.data <- pref.data %>%
+  mutate(correct_inf = ifelse(pick == corObjInf,1,0),
+         correct_prior = ifelse(pick == corObjPrior,1,0)) 
 # check resulting datafile
 str(pref.data)
 head(pref.data)
 # write csv file for further analysis
-write.csv(pref.data, file="pref.data.csv")
+write.csv(pref.data, file="ex3.pref.data.csv")
 
 ################################################################################################################
 
