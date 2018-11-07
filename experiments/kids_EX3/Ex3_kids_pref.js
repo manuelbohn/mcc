046d06540b1618f904b8a1ff28698c29ee66d73d
manuelbohn <bohn@stanford.edu>
2018-11-06 16:32:52 -0800

    elaborating on instructions for kids ex 3

--- experiments/kids_EX3/Ex3_kids_pref.js
+++ experiments/kids_EX3/Ex3_kids_pref.js
@@ -593,7 +593,7 @@ var targetSwitch = trainTargetSwitch.concat(testTargetSwitch)
 
 
 
-var fruits = shuffle(["t01","t02","t03","t04","t05","t06","t07","t08","t09","t10", "t11","t12","t13","t14","t15","t16","t17","t18","t19","t20","t21","t22"]);
+var fruits = shuffle(["t01","t02","t03","t04","t05","t07","t08","t09","t10", "t11","t12","t13","t14","t16","t17","t18","t19","t20","t21","t22"]);
 // randomizing order and combiantion of test objects
 var rightFruit = fruits.sort(() => .5 - Math.random()).slice(0,10);
 var remainingFruits = $.grep(fruits, function(value) {
